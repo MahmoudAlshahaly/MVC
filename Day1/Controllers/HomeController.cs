3876e3e6a86e02e2576f88dcd2ecb288a3f28751
mahmoudalshahaly <you@example.com>
2023-03-09 13:30:38 +0200

    day1

---
+++ Day1/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Day1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ModelProduct pro;
+
+        public HomeController(ModelProduct _pro)
+        {
+            pro = _pro;
+        }
+        public IActionResult GetAll()
+        {
+            return View("index", pro.GetAll());
+        }
+        public IActionResult GetByID(int id)
+        {
+            return View("ONEPRODUCT", pro.GetByID(id));
+        }
+    }
+}
---
+++ Day1/Models/ModelProduct.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day1.Models
+{
+    public class product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public decimal price { get; set; }
+        public string image { get; set; }
+    }
+    public class ModelProduct
+    {
+        List<product> lstpro = new List<product>() { 
+        new product{id=1,name="iphone 11",description="ram 16",price=1000,image="1.jpg"},
+        new product{id=2,name="iphone 16",description="ram 32",price=2000,image="2.jpg"},
+        new product{id=3,name="iphone 10",description="ram 12",price=3000,image="3.jpg"},
+        new product{id=4,name="iphone 6",description="ram 8",price=4000,image="4.jpg"},
+        new product{id=5,name="iphone 7",description="ram 14",price=5000,image="5.jpg"},
+       
+        };
+        public List<product> GetAll()
+        {
+            return lstpro;
+        }
+        public product GetByID(int id)
+        {
+            return lstpro.Find(a=>a.id==id);
+        }
+    }
+}
